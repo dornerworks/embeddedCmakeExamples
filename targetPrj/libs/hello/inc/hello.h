324db7b99b8ecf9491f312567d63301fe5647668
Nicholas Pahl <nicholas.pahl@dornerworks.com>
2019-10-27 17:29:06 -0400

    Copies basePrj to targetPrj

---
+++ targetPrj/libs/hello/inc/hello.h
@@ -0,0 +1,7 @@
+#ifndef _HELLO_H_
+#define _HELLO_H_
+
+void PrintHelloWorld();
+
+#endif
+
---
+++ targetPrj/libs/hello/src/hello.c
@@ -0,0 +1,10 @@
+#include "hello.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintHelloWorld()
+{
+    printf("Hello World!\n");
+}
+
